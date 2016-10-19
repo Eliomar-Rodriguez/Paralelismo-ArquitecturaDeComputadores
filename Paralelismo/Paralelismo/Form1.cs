abaf0e080117060ad78300f788704061964a708c
Eliomar Rodríguez <Eliomar Rodrguez>
2016-10-18 20:52:52 -0600

    Subiendo los 3 archivos a utilizar, trabaje en ventana principal (soo disenio)

--- Paralelismo/Paralelismo/Form1.cs
+++ Paralelismo/Paralelismo/Form1.cs
@@ -16,5 +16,41 @@ namespace Paralelismo
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbOps.SelectedIndex == 0)
+            {
+                panel0.Visible = true;
+                //panel0.Enabled = true;
+            }
+            else if (cmbOps.SelectedIndex == 1)
+            {
+                panel1.Visible = true;
+                //panel1.Enabled = true;
+            }
+            else if (cmbOps.SelectedIndex == 2)
+            {
+                panel2.Visible = true;
+                //panel2.Enabled = true;
+            }
+        }
+
+        private void panel0_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel0.Visible = false;
+            panel1.Visible = false;
+            panel2.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
